9298576fda2ecb43ead5646ebe60eb469e31b3c5
yulihzn <330193303@qq.com>
2016-05-26 19:30:39 +0800

    now i can get the lines

--- core/src/com/mw/actor/MapShadow.java
+++ core/src/com/mw/actor/MapShadow.java
@@ -25,7 +25,7 @@ public class MapShadow extends Actor{
     private ShapeRenderer shapeRenderer;
     private GridPoint2 sightPosIndex = new GridPoint2(0,0);
     private int width = 0,height = 0;
-    private int sightRadius = 6;
+    private int sightRadius = 5;
     private Array<EdgeLine> lines = new Array<EdgeLine>();
     private int[][] dungeonArray;
     //test
@@ -79,40 +79,31 @@ public class MapShadow extends Actor{
         for (int i = 0;i < lines.size;i++){
             EdgeLine ed = lines.get(i);
             shapeRenderer.setColor(Color.GREEN);
-            shapeRenderer.line(ed.getStart().x,ed.getStart().y,ed.getEnd().x,ed.getEnd().y);
-            if(ed.getNext() != -1){
+            shapeRenderer.line(ed.getStart().x, ed.getStart().y, ed.getEnd().x, ed.getEnd().y);
+            if(ed.getPrev() != -1&&ed.getNext() != -1) {
+                shapeRenderer.setColor(Color.BLUE);
                 EdgeLine en = lines.get(ed.getNext());
-                shapeRenderer.setColor(Color.BLUE);
                 shapeRenderer.line(ed.getEnd().x,ed.getEnd().y,en.getStart().x,en.getStart().y);
-            }
-            if(ed.getPrev() != -1){
-                shapeRenderer.line(ed.getStart().x,ed.getStart().y,ed.getEnd().x,ed.getEnd().y);
+                shapeRenderer.setColor(Color.ORANGE);
                 EdgeLine ep = lines.get(ed.getPrev());
-                shapeRenderer.setColor(Color.BLUE);
                 shapeRenderer.line( ep.getEnd().x,ep.getEnd().y,ed.getStart().x,ed.getStart().y);
             }
         }
-//        if(lines.size>0){
-//            shapeRenderer.setColor(Color.BLUE);
-//            EdgeLine ed = lines.get(0);
-//            int next = ed.getNext();
-//            int pre = ed.getPrev();
-//            shapeRenderer.line(ed.getStart().x,ed.getStart().y,ed.getEnd().x,ed.getEnd().y);
-//            while (next > 0){
-//                EdgeLine en = lines.get(next);
-//                shapeRenderer.line(ed.getEnd().x,ed.getEnd().y,en.getStart().x,en.getStart().y);
-//                shapeRenderer.line(en.getStart().x,en.getStart().y,en.getEnd().x,en.getEnd().y);
-//                next = en.getNext();
-//                ed = en;
-//            }
-//            while (pre > 0){
-//                EdgeLine ep = lines.get(pre);
-//                shapeRenderer.line(ep.getEnd().x,ep.getEnd().y,ed.getStart().x,ed.getStart().y);
-//                shapeRenderer.line(ep.getStart().x,ep.getStart().y,ep.getEnd().x,ep.getEnd().y);
-//                pre = ep.getPrev();
-//                ed = ep;
-//            }
-//        }
+        if(lines.size>0){
+            shapeRenderer.setColor(Color.PURPLE);
+            EdgeLine ed = lines.get(0);
+            int next = ed.getNext();
+            int i = next;
+            shapeRenderer.line(ed.getStart().x,ed.getStart().y,ed.getEnd().x,ed.getEnd().y);
+            while (next > 0||next == i){
+                Gdx.app.log("next",next+"");
+                EdgeLine en = lines.get(next);
+                shapeRenderer.line(ed.getEnd().x,ed.getEnd().y,en.getStart().x,en.getStart().y);
+                shapeRenderer.line(en.getStart().x,en.getStart().y,en.getEnd().x,en.getEnd().y);
+                next = en.getNext();
+                ed = en;
+            }
+        }
         shapeRenderer.end();
 
         Gdx.gl.glDisable(GL20.GL_BLEND);
@@ -143,6 +134,12 @@ public class MapShadow extends Actor{
         edgeLine.setDistance(Vector2.dst(sx,sy,dx,dy));
         edgeLine.setId(id);
         return edgeLine;
+    }
+
+    private boolean isBlock(int i,int j){
+        return !(dungeonArray[i][j] != Dungeon.tileStoneWall
+                &&dungeonArray[i][j]!= Dungeon.tileDirtWall
+                &&dungeonArray[i][j]!= Dungeon.tileUnused);
     }
 
     public void updateLines(){
@@ -186,63 +183,43 @@ public class MapShadow extends Actor{
                 y2 = y+1*32;
                 x3 = x+1*32;
                 y3 = y;
-                if(dungeonArray[i][j] != Dungeon.tileStoneWall
-                        &&dungeonArray[i][j]!= Dungeon.tileDirtWall
-                        &&dungeonArray[i][j]!= Dungeon.tileUnused){
-                    if(!(i==iMin||i==iMax-1||j==jMin||j==jMax-1)){
+                //添加边围住视野
+                if(j==jMin&&i!=iMax-1&&i!=iMin){//中心点下层
+                    lines.add(getEdgeLine(x1,y1,x2,y2));
+                }
+                if(i==iMin&&j!=jMax-1&&j!=jMin){//中心点左层
+                    lines.add(getEdgeLine(x2,y2,x3,y3));
+                }
+                if(j==jMax-1&&i!=iMax-1&&i!=iMin){//中心点上层
+                    lines.add(getEdgeLine(x3,y3,x,y));
+                }
+                if(i==iMax-1&&j!=jMax-1&&j!=jMin){//中心点右层
+                    lines.add(getEdgeLine(x,y,x1,y1));
+                }
+                if(i==iMin||j==jMin||i==iMax-1||j==jMax-1){
+                    continue;//如果是边就跳过
+                }
+                if(!isBlock(i,j)){
+                    continue;//如果不是视野障碍物就跳过
+                }
 
-                        continue;//如果不是视野障碍物而且不是边就跳过
-                    }
-                }
-                /**这里 边被照亮的情况有八种**/
-                if(sx < x && sy < y){
-                    //左下
-                    // Px,y->Px1,y1
-                    // Px3,y3->Px,y
-                    lines.add(getEdgeLine(x,y,x1,y1));
-                    lines.add(getEdgeLine(x3,y3,x,y));
-                }
-                else if(sx <= x && sy >= y && sy <= y1){
-                    //左中
-                    // Px,y->Px1,y1
-                    lines.add(getEdgeLine(x,y,x1,y1));
-                }
-                else if(sx < x1 && sy > y1){
-                    //左上
-                    // Px,y->Px1,y1
-                    // Px1,y1->Px2,y2
-                    lines.add(getEdgeLine(x,y,x1,y1));
+                if(sy >= y1&&!isBlock(i,j+1)){
+                    //top
                     lines.add(getEdgeLine(x1,y1,x2,y2));
                 }
-                else if(sx >= x1 && sy >= y1 && sx <= x2){
-                    //上中
-                    // Px1,y1->Px2,y2
-                    lines.add(getEdgeLine(x1,y1,x2,y2));
-                }
-                else if(sx > x2 && sy > y2){
-                    //右上
-                    // Px1,y1->Px2,y2
-                    // Px2,y2->Px3,y3
-                    lines.add(getEdgeLine(x1,y1,x2,y2));
-                    lines.add(getEdgeLine(x2,y2,x3,y3));
-                }
-                else if(sx >= x2 && sy <= y2 && sy >= y3){
-                    //右中
-                    // Px2,y2->Px3,y3
-                    lines.add(getEdgeLine(x2,y2,x3,y3));
-                }
-                else if(sx > x3 && sy < y3){
-                    //右下
-                    // Px2,y2->Px3,y3
-                    // Px3,y3->Px,y
-                    lines.add(getEdgeLine(x2,y2,x3,y3));
+                if(sy <= y&&!isBlock(i,j-1)){
+                    //bottom
                     lines.add(getEdgeLine(x3,y3,x,y));
                 }
-                else if(sx >= x && sy <= y && sx <= x3){
-                    //下中
-                    // Px3,y3->Px,y
-                    lines.add(getEdgeLine(x3,y3,x,y));
+                if(sx <= x&&!isBlock(i-1,j)){
+                    //left
+                    lines.add(getEdgeLine(x,y,x1,y1));
                 }
+                if(sx >= x2&&!isBlock(i+1,j)){
+                    //right
+                    lines.add(getEdgeLine(x2,y2,x3,y3));
+                }
+
             }
         }
         connectEdges();
@@ -274,111 +251,6 @@ public class MapShadow extends Actor{
             }
         }
     }
-    private void calculateEdges(){
-        int sx = (sightPosIndex.x*32)+16;//视野的横坐标
-        int sy = (sightPosIndex.y*32)+16;//视野的纵坐标
-        //切割边缘
-        points.clear();
-        for (int k = 0;k < lines.size;k++) {
-            EdgeLine e = lines.get(k);
-            if(e.getNext() == -1){//当前线段的下一条不存在
-                for (int i = k+1; i < lines.size; i++) {//从列表当前线段的下一条开始读取
-                    EdgeLine ed = lines.get(i);
-                    if( i==k){
-                        continue;
-                    }
-                    //获取下一条线段是否和当前线段有交点
-                    Vector2 p = getIntersection(sx,sy,e.getEnd().x,e.getEnd().y,
-                            ed.getStart().x,ed.getStart().y,ed.getEnd().x,ed.getEnd().y);
-                    //如果有交点，下一条的头是交点，设置当前线段的下一条
-                    if(p.x != -1 && p.y != -1){
-                        points.add(p);
-                        ed.getStart().x = p.x;
-                        ed.getStart().y = p.y;
-                        e.setNext(i);
-                        ed.setPrev(k);
-                        break;
-                    }
-                }
-            }
-            if(e.getPrev() == -1){
-                for (int i = k+1; i < lines.size; i++) {
-                    EdgeLine ed = lines.get(i);
-                    if(i==k){
-                        continue;
-                    }
-                    Vector2 p = getIntersection(sx,sy,e.getStart().x,e.getStart().y,
-                            ed.getStart().x,ed.getStart().y,ed.getEnd().x,ed.getEnd().y);
-                    if(p.x != -1 && p.y != -1){
-                        points.add(p);
-                        ed.getEnd().x = p.x;
-                        ed.getEnd().y = p.y;
-                        e.setPrev(i);
-                        ed.setNext(k);
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    private Vector2 getIntersection(double x1,double y1,double x2,double y2,double x3,double y3,double x4,double y4){
-        try {
-            if((x2 == x3&&y2==y3) || (x2 == x4&&y2==y4)){//重合
-                return new Vector2(-1,-1);
-            }
-            if(x1 - x2 == 0){
-                return  new Vector2(-1,-1);
-            }
-            //第一条直线
-            double a = (y1 - y2) / (x1 - x2);
-            double b = (x1 * y2 - x2 * y1) / (x1 - x2);
-//            System.out.println("求出该直线方程为: y=" + a + "x + " + b);
-            if(x3 - x4 == 0){
-                return  new Vector2(-1,-1);
-            }
-            //第二条
-            double c = (y3 - y4) / (x3 - x4);
-            double d = (x3 * y4 - x4 * y3) / (x3 - x4);
-//            System.out.println("求出该直线方程为: y=" + c + "x + " + d);
-            if((x3 - x4) * (y1 - y2) - (x1 - x2) * (y3 - y4) == 0){
-                return  new Vector2(-1,-1);
-            }
-
-            double x = ((x1 - x2) * (x3 * y4 - x4 * y3) - (x3 - x4) * (x1 * y2 - x2 * y1))
-                    / ((x3 - x4) * (y1 - y2) - (x1 - x2) * (y3 - y4));
-
-            double y = ((y1 - y2) * (x3 * y4 - x4 * y3) - (x1 * y2 - x2 * y1) * (y3 - y4))
-                    / ((y1 - y2) * (x3 - x4) - (x1 - x2) * (y3 - y4));
-
-//            System.out.println("他们的交点为: (" + x + "," + y + ")");
-            //判断是否在线段上
-            boolean isOnSegment = true;
-            if((x > x3 && x > x4) ||(x < x3 && x < x4)){
-                isOnSegment = false;
-            }
-            if((y > y3 && y > y4) ||(y < y3 && y < y4)){
-                isOnSegment = false;
-            }
-            //判断是否在射线上
-            //保持x,y和x2,y2在同一象限
-            if((x1 - x2 > 0 && x1 - x < 0)
-                    ||(x1 - x2 < 0 && x1 - x > 0)
-                    ||(y1 - y2 > 0 && y1 - y < 0)
-                    ||(y1 - y2 < 0 && y1 - y > 0)){
-                isOnSegment = false;
-            }
-
-            if(!isOnSegment){
-                x = -1;
-                y = -1;
-            }
-            return  new Vector2((float) x,(float)y);
-        }catch (Exception e){
-            Gdx.app.log("exec","除0异常");
-            return  new Vector2(-1,-1);
-        }
-    }
-
     private void calculateProjections(){
         int sx = (sightPosIndex.x*32)+16;//视野的横坐标
         int sy = (sightPosIndex.y*32)+16;//视野的纵坐标
